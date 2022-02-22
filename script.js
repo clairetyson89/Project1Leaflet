3107938ae709f77f235c005e243fdc238cc72cac
Claire <70619108+clairetyson89@users.noreply.github.com>
2022-02-21 22:20:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-var map = L.map('map').setView([29.74487111897466, -99.59174995408246], 3);
 
- L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-	maxZoom: 13
-}).addTo(map);
-
-
-$.getJSON("https://raw.githubusercontent.com/clairetyson89/Project1Leaflet/main/aqua.geojson",function(data){
-	var bodyIcon = L.icon({
-		iconUrl: "https://cdn.iconscout.com/icon/premium/png-256-thumb/fish-bowl-2792692-2325270.png",
-		iconSize: [30,30]
-	});
-    L.geoJson(data ,{
-    pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: bodyIcon});
-    }
-  } ).addTo(map);
-});
